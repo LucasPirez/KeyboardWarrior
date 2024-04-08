6fd5e1832d2b64044e15e4170c54cf27a07ee8bc
Lucas Pirez <lucaspirez42@gmail.com>
2024-04-08 12:10:34 -0300

    add generic in SokectResponseDTO

--- BackEnd/keyboard-warrior/DTOs/SocketResponseDTO.cs
+++ BackEnd/keyboard-warrior/DTOs/SocketResponseDTO.cs
@@ -1,10 +1,29 @@
 ﻿namespace keyboard_warrior.DTOs
 {
 
-    public class SocketResponseDTO
+    public class SocketResponseDTO<T>
     {
-        public int code { get; set; } = 200;
-        public string message { get; set; } = string.Empty;
-        public object data { get; set; } = new() { };
+        public int Code { get; set; } = 200;
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+
+        public SocketResponseDTO() { }
+        public SocketResponseDTO(int code,string message,T data)
+        { 
+            Code = code;
+            Message = message; 
+            Data = data;
+        }
+
+        public SocketResponseDTO<T> Send(int code, string message, T data)
+        {
+            return new SocketResponseDTO<T>
+            {
+                Code = code,
+                Message = message,
+                Data = data
+            };
+        }
     }
 }
