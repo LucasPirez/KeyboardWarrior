2b4a3193930ba4fafe07e7715c158d621bef970c
Lucas Pirez <lucaspirez42@gmail.com>
2024-04-25 20:15:20 -0300

    add xUnit test project

---
+++ BackEnd/Keyboard-warrior.Test/TestStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Keyboard_warrior.Test
+{
+    public class TestStartup
+    {
+        public IServiceCollection Services { get; }
+
+        public TestStartup()
+        {
+            Services = new ServiceCollection();
+            Services.AddScoped( provider =>
+            {
+             /*   var handler = provider.GetRequiredService<HttpMessageHandler>();*/
+                var connection = new HubConnectionBuilder()
+                    .WithUrl("https://localhost:7088/Play", o =>
+                    {
+                        o.Headers.Add("Access-Control-Allow-Origin", "*");
+                    })
+                    .Build();
+
+                connection.StartAsync().Wait();
+                return connection;
+            });
+        }
+    }
+    [CollectionDefinition("IntegrationTest")]
+    public class IntegrationTestCollection : ICollectionFixture<TestStartup> { }
+}
